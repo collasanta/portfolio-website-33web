993e7bfaa0077d9adcbf35991b05801007d9db30
Victor Henrique Collasanta <victor.collasanta@gmail.com>
2023-04-20 14:16:47 -0300

    fix

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,14 +2,13 @@ import email from '../assets/email.png';
 import mobile from '../assets/mobile.png';
 import logosnake from '../assets/logosnake.png'
 import react from '../assets/react.png';
-import ethereum from '../assets/ethereum.PNG';
 import solidity from '../assets/solidity.png';
 import logoclean from '../assets/logoclean.png';
 import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
 import about03 from '../assets/about03.png';
 import about04 from '../assets/about04.png';
-import logo from '../assets/logo.PNG';
+import logo from '../assets/logo.png';
 import adidas from '../assets/adidas.png';
 import asus from '../assets/asus.png';
 import nb from '../assets/nb.png';
@@ -46,7 +45,6 @@ export default {
   adidas,
   asus,
   nb,
-  ethereum,
   solidity,
   logoclean,
   logosnake,
